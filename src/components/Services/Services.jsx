acd9bb8b33f202573b55015d7be1cc0da14d5f9d
unknown <amiramamdouh320@gmail.com>
2024-07-14 20:08:33 +0300

    Update

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,14 @@
+export default function Services() {
+  return (
+    <>
+      <section className="services">
+        <div className="container text-center py-5">
+          <div className="heading-container">
+            <div className="corner"></div>
+            <h2>Contact Us</h2>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
